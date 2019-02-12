d2c25396b0e4816f52f9cf30b7bb7b89cb6d2d12
VimukthiPerera <ashbournerox@gmail.com>
2019-02-12 15:41:27 +0530

    Fix minor issues

--- pattern-1/bosh-release/jobs/health_check/templates/run.sh
+++ pattern-1/bosh-release/jobs/health_check/templates/run.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+isAlive=0
+
 health_check() {
     # Check if health check endpoint is alive
     if curl --output /dev/null --silent --fail -k "$1"
@@ -10,18 +12,31 @@ health_check() {
 
         # Check if requests to the health check endpoint produces  200 response
         if [[ "$status_code" -ne 200 ]] ; then
-            echo "WSO2 APIM $2 produces an invalid response: $status_code" >>/dev/stderr
+            >&2 echo "WSO2 IS $2 produces an invalid response: $status_code"
             exit 1
         else
-            echo "WSO2 APIM $2 is Running!"
+            echo "WSO2 IS $2 is Running!"
+            isAlive=1
         fi
     else
-        echo "WSO2 APIM $2 is not alive" >>/dev/stderr
-        exit 1
+        >&2 echo "WSO2 IS $2 is not alive. Retrying in 10s..."
+        isAlive=0
     fi
 }
 
 carbonHealthCheckEP="https://localhost:9443/carbon/admin/login.jsp"
+COUNTER=0
 
-health_check ${carbonHealthCheckEP} Carbon
+# While the endpoint is not alive, and the server has been retrying for less than 3 minutes
+while [ ${isAlive} -eq 0 ]&&[ ${COUNTER} -lt 18 ]; do
+    sleep 10s
+    health_check ${carbonHealthCheckEP} Carbon
+    let COUNTER=COUNTER+1
+done
+
+if [ ${isAlive} -eq 0 ]; then
+    >&2 echo "Could not connect to WSO2 IS $2. Exiting..."
+    exit 1
+fi
+
 exit 0
